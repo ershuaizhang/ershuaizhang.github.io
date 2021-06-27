3a55f1c9e67c79c232727d83d871a5d6560b90dd
Desk_PC <1126005094@qq.com>
2021-06-27 22:23:21 +0800

    delete no need files

--- springboot/webProtal/src/main/resources/static/blog/menupage/upmenu.js
+++ springboot/webProtal/src/main/resources/static/blog/menupage/upmenu.js
@@ -25,17 +25,6 @@ function showPicturePage(choose) {
     $("#header").load(location.web.staticpath + '/blog/menupage/upmenu.html');
 }
 
-function showMediaPage(mediaType) {
-    if (!mediaType) {
-        return;
-    }
-    var target = '/blog/mediatype/' + mediaType + 'home.html';
-    $("#content").load(location.web.staticpath + target);
-    $("#left_side").load(location.web.staticpath + "/blog/mediatype/mediamenu/" + mediaType + "menu.html");
-    $("#right_side").load(location.web.staticpath + "/public/html/blank.html");
-    $("#header").load(location.web.staticpath + '/blog/menupage/upmenu.html');
-}
-
 function showMeToolsPage(toolName) {
     if (!toolName) {
         return;
